a3ac3a2cc35670c62e280cbda3286bf66f9bfe18
Anatolii Rakovskii <rtolik@yandex.ru>
2016-11-21 14:01:24 +0300

    Initial version of grid with simple test

---
+++ client/js/grid.js
@@ -0,0 +1,259 @@
+$.widget("sokol.grid", {
+    options: {
+        url: null,
+        id: "",
+        pageSize: 20,
+        offset: 0,
+        pageCount: 0,
+        currentPage: 1,
+        total: 0,
+        title: "",
+        columnsVisible: []
+    },
+
+    _create: function () {
+        this.createList();
+    },
+
+    _setOptions: function (options) {
+        this._super(options);
+        this.reload();
+    },
+
+    createList: function() {
+        var central = this.element;
+        central.empty();
+
+        var pagination = this.createPagination(central);
+        this.createColumnsSelector(pagination);
+
+        this.renderTablePanel();
+        this.reload();
+
+        this.createPagination(central);
+    },
+
+    setPage: function(page) {
+        if (page > 0 && page < this.options.pageCount + 1) {
+            this.options.currentPage = page;
+            this.options.offset = this.options.pageSize * (page - 1);
+            this.reload();
+        }
+    },
+
+    nextPage: function() {
+        this.setPage(this.options.currentPage + 1)
+    },
+
+    previewPage: function() {
+        this.setPage(this.options.currentPage - 1);
+    },
+
+    refresh: function () {
+        this.renderTableHeader();
+        this.renderRows();
+        this.updatePagination();
+    },
+
+    reload: function() {
+        if (this.options.url) {
+            $.getJSON(this.options.url, {
+                id: this.options.id,
+                size: this.options.pageSize,
+                offset: this.options.offset
+            }, $.proxy(function (data) {
+                this.options.data = data.data;
+                this.options.total = data.total;
+                this.refresh();
+
+            }, this));
+        } else {
+            this.options.total = this.options.data.length;
+            this.refresh();
+        }
+    },
+
+    createPagination: function(container) {
+        var div = $('<div style="text-align1: center; margin-bottom: 10px;"></div>').appendTo(container);
+        $(
+            '<span style="margin-right: 10px;">Найдено: <span name="gridItemsCount">0</span></span>' +
+            '<button name="preview" class="btn btn-default" href = "#" disabled1="disable">' +
+            '<span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span> Предыдущая</button>&nbsp;' +
+            '<select name="pageSelector" class="selectpicker"></select>&nbsp;' +
+            '<button name="next" class="btn btn-default" href = "#">Следующая ' +
+            '<span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span></button>&nbsp;' +
+            '<span style="margin-left: 10px;">Отображать ' +
+            '<select name="pageSize" class="selectpicker"><option>20</option><option>50</option><option>100</option></select>&nbsp;' +
+            '</span>&nbsp;'
+        ).appendTo(div);
+
+        $(div).find('.selectpicker').selectpicker({
+            noneSelectedText: '0 / 0',
+            width: 'fit'
+        });
+        $(div).find('.selectpicker[name="pageSelector"]').on('change', {grid: this}, function(event){
+            var selected = $(this).find("option:selected").val();
+            var val = selected.split(" / ")[0];
+            event.data.grid.setPage(val);
+        });
+        $(div).find('.selectpicker[name="pageSize"]').on('change', {grid: this}, function(event){
+            var size = $(this).find("option:selected").val();
+            var grid = event.data.grid;
+            grid.options.pageSize = size;
+            grid.reload();
+
+        });
+        $(div).find("[name='preview']").on('click', $.proxy(this.previewPage, this));
+        $(div).find("[name='next']").on('click', $.proxy(this.nextPage, this));
+
+        return div;
+    },
+    
+    updatePagination: function() {
+        this.element.find("[name='gridItemsCount']").text(this.options.total);
+        var pages = this.options.total / this.options.pageSize;
+        var pagesInt = parseInt(pages);
+        pages = pagesInt < pages ? pagesInt + 1 : pagesInt;
+        this.options.pageCount = pages;
+        if (this.options.currentPage > pages) {
+            this.options.currentPage = pages;
+        }
+
+        var selectors = this.element.find("[name='pageSelector']");
+        selectors.empty();
+        for (var p = 1; p <= pages; p++) {
+            selectors.append($("<option>" + p + " / " + pages + "</option>"))
+        }
+
+        if (this.options.currentPage == 1) {
+            this.element.find("[name='preview']").attr('disabled', 'disabled');
+        } else {
+            this.element.find("[name='preview']").removeAttr('disabled');
+        }
+        if (this.options.currentPage == this.options.pageCount) {
+            this.element.find("[name='next']").attr('disabled', 'disabled');
+        } else {
+            this.element.find("[name='next']").removeAttr('disabled');
+        }
+        this.element.find('select[name="pageSelector"]').val(this.options.currentPage + " / " + this.options.pageCount);
+        this.element.find('select[name="pageSize"]').val(this.options.pageSize);
+        this.element.find('.selectpicker').selectpicker('refresh');
+    },
+
+    renderTableHeader: function() {
+        var thead = this.element.find('[name="tableHead"]');
+        thead.empty();
+        var columns = this.options.columns;
+        var header = $("<tr></tr>");
+        header.appendTo(thead);
+        for (var i = 0; i < columns.length; i++) {
+            var col = columns[i];
+            var colType = columns[i].type;
+            if (colType != "hidden" && this.isColumnVisible(col.id)) {
+                var th = $("<th>" + col.title + "</th>");
+                th.appendTo(header);
+            }
+        }
+    },
+
+    renderTablePanel: function() {
+        var panel = this.element.find('[name="tablePanel"]');
+        if (!panel.length) {
+            panel = $('<div name="tablePanel" class="panel panel-default"></div>');
+            panel.appendTo(this.element);
+        } else {
+            panel.empty();
+        }
+        var panelHeader = $('<div class="panel-heading"></div>');
+        var panelTitle = $('<div class="panel-title">' + this.options.title + '</div>');
+        panelTitle.appendTo(panelHeader);
+        panelHeader.appendTo(panel);
+        var table = $("<table class='table'></table>");
+        table.appendTo(panel);
+        var thead = $('<thead name="tableHead"></thead>');
+        thead.appendTo(table);
+        var tbody = $('<tbody name="tableBody"></tbody>');
+        tbody.appendTo(table);
+
+        this.renderTableHeader();
+    },
+
+    isColumnVisible: function(columnId) {
+        return this.options.columnsVisible.indexOf(columnId) >= 0;
+    },
+
+    renderRows: function() {
+        var tbody = this.element.find('tbody');
+        tbody.empty();
+        var rowsData = this.options.data;
+        var columns = this.options.columns;
+        for (var j = 0; j < rowsData.length; j++) {
+            var row = $("<tr></tr>");
+            var rowObj = rowsData[j];
+            for (var k = 0; k < columns.length; k++) {
+                var colId = columns[k].id;
+                var colType = columns[k].type;
+                if (colType != "hidden" && this.isColumnVisible(colId)) {
+                    if (colId == "title") {
+                        var td = $('<td><a href="#' + rowObj.id + '" target="_blank">' + rowObj[colId] + '</a></td>');
+                        td.appendTo(row);
+                    } else {
+                        var val = rowObj[colId];
+
+                        var td = $('<td>' + (val ? val : '') + '</td>');
+                        td.appendTo(row);
+                    }
+                }
+            }
+            row.appendTo(tbody);
+        }
+    },
+
+    createColumnsSelector: function(element) {
+        var selector = $('<div class="dropdown btn-group">'+
+            '<button type="button" class="btn btn-default btn dropdown-toggle" data-toggle="dropdown">Колонки <span class="caret"></span></button>'+
+            '<ul name="columns" class="dropdown-menu">'+
+            '</ul>'+
+            '</div>');
+
+        var ul = selector.find('[name="columns"]');
+
+        for (var i = 0; i < this.options.columns.length; i++) {
+            var column = this.options.columns[i];
+            if (column.type != "hidden") {
+                var li = $('<li><a href="#" class="" data-value="' + column.id + '" tabIndex="-1"><input type="checkbox"/>&nbsp;' + column.title + '</a></li>');
+                var inp = li.find("input");
+                if (this.options.columnsVisible.indexOf(column.id) > -1) {
+                    inp.prop('checked', true);
+                }
+                ul.append(li);
+            }
+        }
+
+        selector.appendTo(element);
+        $(selector).find('.dropdown-menu a').on('click', $.proxy(function(event) {
+
+            var $target = $(event.currentTarget),
+                val = $target.attr('data-value'),
+                $inp = $target.find('input'),
+                idx;
+
+            if (( idx = this.options.columnsVisible.indexOf(val) ) > -1) {
+                this.options.columnsVisible.splice(idx, 1);
+                setTimeout(function () {
+                    $inp.prop('checked', false);
+                }, 0);
+            } else {
+                this.options.columnsVisible.push(val);
+                setTimeout(function () {
+                    $inp.prop('checked', true);
+                }, 0);
+            }
+
+            $(event.target).blur();
+
+            this.reload();
+            return false;
+        }, this));
+    }
+});
---
+++ client/test/grid.test.js
@@ -0,0 +1,337 @@
+
+var modules;
+modules = modules || {};
+
+modules["grid"] = (function() {
+
+    var rs = {};
+
+    rs.list = function(id, size, offset) {
+        var r = [];
+
+        var total = 55;
+
+        for (var i = offset + 1; i < size + offset + 1 && i <= total; i++) {
+            r.push({
+                id: "id" + i,
+                documentNumber: i.toString(),
+                type: "Test",
+                status: "Draft",
+                title: "Document " + i,
+                registrationDate: this.dateFormat(new Date (1479723927756), "%Y.%m.%d %H:%M", false)
+            });
+        }
+
+        return {
+            data: r,
+            total: total,
+            offset: offset
+        };
+    };
+
+    rs.dateFormat = function(date, fstr, utc) {
+        utc = utc ? 'getUTC' : 'get';
+        return fstr.replace (/%[YmdHMS]/g, function (m) {
+            switch (m) {
+                case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
+                case '%m': m = 1 + date[utc + 'Month'] (); break;
+                case '%d': m = date[utc + 'Date'] (); break;
+                case '%H': m = date[utc + 'Hours'] (); break;
+                case '%M': m = date[utc + 'Minutes'] (); break;
+                case '%S': m = date[utc + 'Seconds'] (); break;
+                default: return m.slice (1); // unknown code, remove %
+            }
+            // add leading zero if required
+            return ('0' + m).slice (-2);
+        });
+    };
+
+    $.getJSON = function(url, params, callback) {
+        setTimeout(function() {
+            callback(rs.list(params.id, parseInt(params.size), parseInt(params.offset)))
+        }, 0);
+    };
+
+    var options = {
+        url: "/gridtest",
+        columnsVisible: [
+                "id",
+                "documentNumber",
+                "type",
+                "status",
+                "title",
+                "registrationDate"
+        ],
+        "columns": [
+            {
+                "id": "id",
+                "type": "hidden"
+            },
+            {
+                "id": "documentNumber",
+                "title": "Номер"
+            },
+            {
+                "id": "type",
+                "title": "Тип",
+                "render": "doctype"
+            },
+            {
+                "id": "status",
+                "title": "Статус"
+            },
+            {
+                "id": "title",
+                "title": "Заголовок"
+            },
+            {
+                "id": "registrationDate",
+                "title": "Дата регистрации",
+                "render": "datetime"
+            }
+        ]
+    };
+
+    var optionsC = {
+        columnsVisible: [
+            "id",
+            "documentNumber",
+            "type",
+            "status",
+            "title",
+            "registrationDate"
+        ],
+        title: "Columns",
+        "columns": [
+            {
+                "id": "id",
+                "type": "hidden"
+            },
+            {
+                "id": "documentNumber",
+                "title": "Номер"
+            },
+            {
+                "id": "type",
+                "title": "Тип",
+                "render": "doctype"
+            },
+            {
+                "id": "status",
+                "title": "Статус"
+            },
+            {
+                "id": "title",
+                "title": "Заголовок"
+            },
+            {
+                "id": "registrationDate",
+                "title": "Дата регистрации",
+                "render": "datetime"
+            }
+        ],
+        "data" : [ {
+            "id" : "b0cf186d-ae9c-44e0-93db-d4d2a72fc88b",
+            "documentNumber" : 1,
+            "type" : "test",
+            "status" : "Зарегистрирован",
+            "title" : "Test document 2",
+            "registrationDate" : "09.04.2016 15:43"
+        }, {
+            "id" : "04b77f61-f92c-4e44-9559-c76261ce3d1e",
+            "documentNumber" : 2,
+            "type" : "Входящий",
+            "status" : "Зарегистрирован",
+            "title" : "Тестовый документ 12",
+            "registrationDate" : "13.04.2016 22:44"
+        }, {
+            "id" : "7bb0fc4f-efbe-4845-81a7-59cbafd303bf",
+            "documentNumber" : "",
+            "type" : "Входящий",
+            "status" : "Зарегистрирован",
+            "title" : "43545656-1111",
+            "registrationDate" : "03.05.2016 13:40"
+        }, {
+            "id" : "e38b0d70-fc25-4e88-b072-e20611e76d73",
+            "documentNumber" : "",
+            "type" : "Исходящий",
+            "status" : "Зарегистрирован",
+            "title" : "1111",
+            "registrationDate" : "03.05.2016 17:58"
+        }, {
+            "id" : "70c17fa8-f7b2-47a5-a57b-02220253fb8e",
+            "documentNumber" : "",
+            "type" : "Внутренний",
+            "status" : "Зарегистрирован",
+            "title" : "Документ документ",
+            "registrationDate" : "03.05.2016 17:26"
+        }, {
+            "id" : "10984d8e-8e18-4384-95bd-8743d1f98676",
+            "documentNumber" : null,
+            "type" : "Входящий",
+            "status" : "Зарегистрирован",
+            "title" : "",
+            "registrationDate" : "13.04.2016 22:03"
+        }, {
+            "id" : "57b5cfa7-9dfb-40a4-8044-8f3fb6a71a83",
+            "documentNumber" : null,
+            "type" : "Входящий",
+            "status" : "Зарегистрирован",
+            "title" : "Еще один документ",
+            "registrationDate" : "13.04.2016 22:45"
+        }, {
+            "id" : "20a8fac1-cebd-455a-8eb6-ce34e59c3631",
+            "documentNumber" : null,
+            "type" : "Входящий",
+            "status" : "Зарегистрирован",
+            "title" : "1235",
+            "registrationDate" : "23.04.2016 00:03"
+        }, {
+            "id" : "d0e38e2e-18bd-48fd-91ec-5e102519cd06",
+            "documentNumber" : "Б12",
+            "type" : "Входящий",
+            "status" : "Зарегистрирован",
+            "title" : "Test document 123",
+            "registrationDate" : "30.04.2016 13:44"
+        }, {
+            "id" : "ec8b9829-6fcb-4b7f-9356-fc26b38dff10",
+            "documentNumber" : "АА123",
+            "type" : "Входящий",
+            "status" : "Зарегистрирован",
+            "title" : "Входящий ec8b9829",
+            "registrationDate" : "04.05.2016 22:52"
+        } ]
+    };
+
+    var testComponent = null;
+
+    var assertEquals = function(expected, real, message) {
+        if (expected !== real) {
+            throw new Error(message + " : " + expected + " != " + real);
+        }
+    };
+
+    var testAll = function() {
+
+        var assertTableStateCorrect = function(count, rowCount, page, pageCount, firstDoc, secondDoc, lastDoc) {
+
+            if (count != undefined) {
+                var countElements = testComponent.element.find('[name="gridItemsCount"]');
+                assertEquals(count, parseInt(countElements.first().text()), "Wrong first total count");
+                assertEquals(count, parseInt(countElements.last().text()), "Wrong last total count");
+            }
+
+            var rows = testComponent.element.find('[name="tableBody"] > tr');
+            if (rowCount != undefined) {
+                assertEquals(rowCount, rows.length, "Wrong rows count");
+            }
+
+            var pageInfo = $(".selectpicker[name='pageSelector']").first().parent().find("button:first").attr("title").split(/[ /]+/g);
+
+            if (page != undefined) {
+                assertEquals(page, parseInt(pageInfo[0]), "Wrong page");
+            }
+
+            if (pageCount != undefined) {
+                assertEquals(pageCount, parseInt(pageInfo[1]), "Wrong page count");
+            }
+
+            if (firstDoc != undefined) {
+                assertEquals(firstDoc, rows.first().text());
+            }
+
+            if (secondDoc != undefined) {
+                assertEquals(secondDoc, rows.eq(1).text());
+            }
+
+            if (lastDoc != undefined) {
+                assertEquals(lastDoc, rows.last().text());
+            }
+
+        };
+
+        var steps = [];
+
+        steps.push({
+            step: function renderTable() {
+                options.title = "Test All";
+                testComponent = $.sokol.grid(options, $("<div class='container'></div>").appendTo("body"));
+            },
+            wait: 500
+        });
+        steps.push({
+            step: function checkTable() {
+                assertTableStateCorrect(55, 20, 2, 3, "1TestDraftDocument 12016.11.21 13:25", "2TestDraftDocument 22016.11.21 13:25", "20TestDraftDocument 202016.11.21 13:25");
+            },
+            wait: 500
+        });
+        steps.push({
+            step: function openPageDialog() {
+                $(".selectpicker[name='pageSize']").first().parent().find("button:first").trigger("click");
+            },
+            wait: 500
+        });
+        steps.push({
+            step: function changePageSize() {
+                $(".selectpicker[name='pageSize']").first().parent().find("ul li:nth-child(2) a").trigger("click");
+            },
+            wait: 500
+        });
+        steps.push({
+            step: function checkPageSize() {
+                assertTableStateCorrect(55, 50);
+            },
+            wait: 500
+        });
+        steps.push({
+            step: function goNextPage() {
+                $("button[name='next']").first().trigger("click");
+            },
+            wait: 500
+        });
+        steps.push({
+            step: function checkNextPage() {
+                assertTableStateCorrect(55, 5, 2, 2, "51TestDraftDocument 512016.11.21 13:25", "52TestDraftDocument 522016.11.21 13:25", "55TestDraftDocument 552016.11.21 13:25");
+            },
+            wait: 500
+        });
+
+        var doAction = function(i, steps) {
+            if (i < steps.length) {
+                if (steps[i].disable) {
+                    doAction(i + 1, steps);
+                } else {
+                    setTimeout(function executeStep() {
+                        console.info("[" + steps[i].step.name + "]");
+                        steps[i].step();
+                        doAction(i + 1, steps);
+                    }, steps[i].wait);
+                }
+            }
+        };
+
+        doAction(0, steps);
+    };
+
+    return {
+        testAll: {
+            title: "Тестировать все",
+            action: function() {
+                testAll();
+            }
+        },
+        showDocuments: {
+            title: "Загрузить выборку \"Документы\"",
+            action: function() {
+                $.sokol.grid(options, $("<div class='container'></div>").appendTo("body"));
+            }
+        },
+        showColumns: {
+            title: "Видимость колонок",
+            action: function() {
+                $.sokol.grid(optionsC, $("<div class='container'></div>").appendTo("body"));
+            }
+        }
+    }
+}());
+
